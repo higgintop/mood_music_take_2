2f7a7bd358508779796eadc4673fef56bc608dc3
Allison Higginbotham <higgintop@gmail.com>
2015-05-21 12:07:19 -0500

    Edited the listing recommendation test to create recommendations with 'happy' and added the exiting of program to get rid of errors from highline

--- test/integration/test_listing_recommendations.rb
+++ test/integration/test_listing_recommendations.rb
@@ -22,8 +22,8 @@ class TestListingRecommendations < Minitest::Test
   end
 
   def test_listing_of_recommendations_happy_path
-    create_recommendation("Elephant", "Tame Impala", "1")
-    create_recommendation("Tangerine", "Led Zeppelin", "1")
+    create_recommendation("Elephant", "Tame Impala", "happy")
+    create_recommendation("Tangerine", "Led Zeppelin", "happy")
     shell_output = ""
     expected = ""
     IO.popen('./mood_music manage', 'r+') do |pipe|
@@ -33,6 +33,12 @@ class TestListingRecommendations < Minitest::Test
       pipe.puts "1"
       expected << "1. Elephant by Tame Impala\n"
       expected << "2. Tangerine by Led Zeppelin\n"
+      pipe.puts "1"
+      expected << "What would you like to do?\n1. edit\n2. delete\n3. exit\n"
+      pipe.puts "3"
+      expected << main_menu
+      pipe.puts "3"
+      expected << "Peace Out!\n"
       pipe.close_write
       shell_output = pipe.read
     end
